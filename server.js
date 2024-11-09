5465538f5452b3e77f11a48c55735a9e62b02298
Krishan_Manohara <krishan.manohara@cube.global>
2024-11-09 11:32:15 +0530

    check route and server request

--- server.js
+++ server.js
@@ -2,7 +2,12 @@ import express from "express";
 import mongoose from 'mongoose';
 import dotenv from "dotenv";
 import userRoute from "./routes/user.route.js"
-
+import gigRoute from "./routes/gig.route.js"
+import orderRoute from "./routes/order.route.js"
+import conversationRoute from "./routes/conversation.route.js"
+import messageRoute from "./routes/message.route.js"
+import reviewRoute from "./routes/review.route.js"
+import authRoute from "./routes/auth.route.js"
 
 const app = express();
 dotenv.config();
@@ -17,7 +22,14 @@ const connect = async () => {
     }
   };
 
-  app.use("/api/user", userRoute);
+  app.use("/api/auths", authRoute)
+  app.use("/api/users", userRoute);
+  app.use("/api/gigs", gigRoute);
+  app.use("/api/orders", orderRoute);
+  app.use("/api/conversations", conversationRoute);
+  app.use("/api/messages", messageRoute);
+  app.use("/api/reviews", reviewRoute);
+ 
 
   app.listen(8800, () => {
     connect();
