e3669b70a2a72a87c439a418b5fc6cf9a9aa6f3d
Hiroshan Victor <hiroshanvictor@gmail.com>
2025-03-12 06:53:03 +0530

    Gig comments & order controller
    Order image = gig.cover --> plan.cover

--- controllers/gig.controller.js
+++ controllers/gig.controller.js
@@ -1,6 +1,7 @@
 import Gig from "../models/gig.model.js";
 import createError from "../utils/createError.js";
 
+// CREATE GIG FUNCTION
 export const createGig = async (req, res, next) => {
   if (!req.isSeller)
     return next(createError(403, "Only sellers can create a gig!"));
@@ -17,6 +18,8 @@ export const createGig = async (req, res, next) => {
     next(err);
   }
 };
+
+// DELETE GIG FUNCTION
 export const deleteGig = async (req, res, next) => {
   try {
     const gig = await Gig.findById(req.params.id);
@@ -29,6 +32,8 @@ export const deleteGig = async (req, res, next) => {
     next(err);
   }
 };
+
+// GET GIG FUNCTION
 export const getGig = async (req, res, next) => {
   try {
     const gig = await Gig.findById(req.params.id);
@@ -38,6 +43,8 @@ export const getGig = async (req, res, next) => {
     next(err);
   }
 };
+
+// GET GIGS BASED ON SEARCH FILTERS
 export const getGigs = async (req, res, next) => {
   const q = req.query;
   const filters = {
--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -21,7 +21,7 @@ export const intent = async (req, res, next) => {
 
   const newOrder = new Order({
     gigId: gig._id,
-    img: gig.cover,
+    img: plan.cover,
     title: gig.title,
     buyerId: req.userId,
     sellerId: gig.userId,
